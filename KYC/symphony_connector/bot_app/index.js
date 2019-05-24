ccbd3f4dce70d8b1eb6e8a5e7bf3e72a8edac870
Julia Zhu <saiyukizn@gmail.com>
2019-05-24 10:06:42 +0800

    Update index.js

--- KYC/symphony_connector/bot_app/index.js
+++ KYC/symphony_connector/bot_app/index.js
@@ -9,24 +9,38 @@ const botHearsRequest = ( event, messages ) => {
 
       let doc = nlp(message.messageText);
 
+      if(message.user.firstName == 'Chengpeng')
+      {
+       let reply_message='Hello Team! Please review verified Trader\'s profile';
+
+;
+      Symphony.sendMessage( message.stream.streamId, reply_message+form, null, Symphony.MESSAGEML_FORMAT);
+
+      }
+
+
+      else{
       /* Find grettings */
       let doc_grettings = doc.match('(hello|hi|bonjour)').out('tags');
       let doc_help = doc.match('(test)').out('tags');
       let reply_message = '';
       if (doc_grettings.length>0) {
-        reply_message = 'Hello ' + message.user.firstName;
+        reply_message = 'Hello ' + message.user.firstName + 'Please check your personal info first.';
         for (var key1 in message.user) {
           reply_message += `\n, ${key1}: ${message.user[key1]}`;
         }
+         rely_message+=' Is it Correct';
       } else if (doc_help.length>0) {
         reply_message = "<messageML>Hello <b>test bold</b>!</messageML>";
       } else {
         reply_message = 'Sorry I don\'t know how to handle this yet. Please wait for our next available assistance to help with that';
       }
+
       Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
 
       /* Detect & analyze request */
       SymphonyBotNLP.findPattern( doc, message );
+    }
 
     })
 }
