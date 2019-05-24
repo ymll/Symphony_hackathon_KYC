2df97fc133aa21d7678eeba8f3aa9b188f62ee55
whtjdals016 <whtjdals016@hanmail.net>
2019-05-24 15:02:29 +0800

    Fix Conflict
    Fix Conflict

--- KYC/symphony_connector/bot_app/index.js
+++ KYC/symphony_connector/bot_app/index.js
@@ -26,30 +26,9 @@ parseUserReply = (index, message) => {
   }
 }
 
-<<<<<<< HEAD
-parseUserReply=(index, message)=>{
-  if(index==0)
-   userStatus.name=message;
- else if(index==1)
-  userStatus.company=message;
- else if(index ==2)
-  userStatus.asset=meesage;
-}
-checkUser = (id,message) => {
-  Symphony.sendMessage( message.stream.streamId, 'next question', null, Symphony.MESSAGEML_FORMAT);
-  if (userStatus.id == null ) {
-    userStatus.id=id;
-    userStatus.index=0;
-    Symphony.sendMessage( message.stream.streamId, 'next question', null, Symphony.MESSAGEML_FORMAT);
-      // 'name':message.user,
-      // 'company':'gs',
-      // 'asset':123,
-      // 'count':0
-=======
 isNewUser = (id) => {
   return userStatus[id] == undefined;
 }
->>>>>>> afaeb7d766cb613f20b8b8538fe75f52d879799a
 
 getQuestion = (message) => {
   id = message.user.userId;
@@ -61,75 +40,9 @@ getQuestion = (message) => {
     // 'asset':123,
     // 'count':0
   } else {
-<<<<<<< HEAD
-  parseUserReply(userStatus[index],message);
-  Symphony.sendMessage( message.stream.streamId, questionTemplate[index], null, Symphony.MESSAGEML_FORMAT);
-  }
-}
-
-/* Callback function when the BOT hears a request */
-const botHearsRequest = ( event, messages ) => {
-
-    messages.forEach( (message, index) => {
-
-      let doc = nlp(message.messageText);
-
-      if(message.user.firstName == 'Julia')
-      {
-        Symphony.sendMessage( message.stream.streamId, "Please check whether trader is verified or not", null, Symphony.MESSAGEML_FORMAT);
-
-        fetch("http://localhost:50000/api/v1/users").then(response => {
-   Symphony.sendMessage( message.stream.streamId, response, null, Symphony.MESSAGEML_FORMAT);
-  if (response.ok) {
-  Symphony.sendMessage( message.stream.streamId, "GET RESPONSE", null, Symphony.MESSAGEML_FORMAT);
-    return response
-  }
-  return Promise.reject(Error('error'))
-}).catch(error => {
-  return Promise.reject(Error(error.message))
-})
-Symphony.sendMessage( message.stream.streamId, "After Fetch", null, Symphony.MESSAGEML_FORMAT);
-
-
-      }
-
-
-      /* Find grettings */
-      let doc_grettings = doc.match('(hello|hi|bonjour)').out('tags');
-      let doc_help = doc.match('(test)').out('tags');
-      let doc_card = doc.match('(card)').out('tags');
-      let doc_upload_file = doc.match('(upload)').out('tags');
-      let reply_message = '';
-      if (doc_grettings.length>0) {
-        reply_message = 'Hello :' + message.user.firstName + 'Please help us to answer some questions.';
-        /*
-        for (var key1 in message.user) {
-          reply_message += `\n, ${key1}: ${message.user[key1]}`;
-        }
-        */
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-        checkUser(message.user.userId,message.messageText.toString());
-
-
-      } else if (doc_help.length>0) {
-        reply_message = "This is a <b>messageML</b>! message"; // No need Message ML Tag
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-      } else if(doc_card.length>0) {
-        reply_message = "<h2>Cards</h2> <card accent=\"tempo-bg-color--blue\" iconSrc=\"./images/favicon.png\"> <header>Card Header. Always visible.</header> <body>Card Body. User must click to view it.</body> </card>";
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-      } else if(doc_upload_file.length>0) {
-        reply_message = "Click this <a href=\"http://localhost:8080\">link</a> to uploade file";
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-      }
-      else {
-        reply_message = 'Sorry I don\'t know how to handle this yet. Please wait for our next available assistance to help with that';
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-      }
-=======
     parseUserReply(userStatus[id], messageText);
     userStatus[id] += 1;
   }
->>>>>>> afaeb7d766cb613f20b8b8538fe75f52d879799a
 
   if (userStatus[id] > Object.keys(questionTemplate).length - 1) {
     return 'Thank you for your information.';
@@ -148,6 +61,22 @@ const botHearsRequest = (event, messages) => {
   messages.forEach((message, index) => {
 
     let doc = nlp(message.messageText);
+
+    if (message.user.firstName == 'Sung Min') {
+      Symphony.sendMessage(message.stream.streamId, "Please check whether trader is verified or not", null, Symphony.MESSAGEML_FORMAT);
+
+      fetch("http://localhost:5000/api/v1/users").then(response => {
+        Symphony.sendMessage(message.stream.streamId, response, null, Symphony.MESSAGEML_FORMAT);
+        if (response.ok) {
+          Symphony.sendMessage(message.stream.streamId, "GET RESPONSE", null, Symphony.MESSAGEML_FORMAT);
+          return response
+        }
+        return Promise.reject(Error('error'))
+      }).catch(error => {
+        return Promise.reject(Error(error.message))
+      })
+      Symphony.sendMessage(message.stream.streamId, "After Fetch", null, Symphony.MESSAGEML_FORMAT);
+    }
 
     /* Find grettings */
     let doc_grettings = doc.match('(hello|hi|bonjour)').out('tags');
@@ -209,16 +138,8 @@ http.createServer(function (req, res) {
       });
     });
   } else {
-<<<<<<< HEAD
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-=======
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(html_utils.generateUploadForm());
->>>>>>> afaeb7d766cb613f20b8b8538fe75f52d879799a
     return res.end();
   }
 }).listen(8080);
