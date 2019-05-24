65f313ddbd30d556d37a889f9603c147fdf1d82f
whtjdals016 <whtjdals016@hanmail.net>
2019-05-24 13:26:26 +0800

    Fix get question bugs
    Fix get question bugs

--- KYC/symphony_connector/bot_app/index.js
+++ KYC/symphony_connector/bot_app/index.js
@@ -5,96 +5,92 @@ const html_utils = require('./html-utils');
 
 const userStatus = {};
 
-const questionTemplate={
- '1':'What\'s your name?',
- '2':'What\'s your company',
- '3':'What\'s your asset amount'
+const questionTemplate = {
+  0: 'What\'s your name?',
+  1: 'What\'s your company',
+  2: 'What\'s your asset amount'
 
 }
 
-parseUserReply=(index, message)=>{
-  if(index==0)
-   userStatus.name=message;
- else if(index==1)
-  userStatus.company=message;
- else if(index=3)
-  userStatus.asset=meesage;
+parseUserReply = (index, message) => {
+  if (index == 0)
+    userStatus.name = message;
+  else if (index == 1)
+    userStatus.company = message;
+  else if (index = 3)
+    userStatus.asset = meesage;
 }
-checkUser = (id,messages) => {
-  Symphony.sendMessage( message.stream.streamId, 'next question', null, Symphony.MESSAGEML_FORMAT);
-  if (userStatus.id == null ) {
-    userStatus.id=id;
-    userStatus.index=0;
-    Symphony.sendMessage( message.stream.streamId, 'next question', null, Symphony.MESSAGEML_FORMAT);
-      // 'name':message.user,
-      // 'company':'gs',
-      // 'asset':123,
-      // 'count':0
+isNewUser = (id) => {
+  return userStatus[id] == undefined;
+}
 
+getQuestion = (message) => {
+  let questionString = '';
+  id = message.user.userId;
+  messageText = message.messageText.toString();
+  questionString += "<br/>Next Question: ";
+  if (isNewUser(id)) {
+    userStatus[id] = 0;
+    // 'name':message.user,
+    // 'company':'gs',
+    // 'asset':123,
+    // 'count':0
   } else {
-    parseUserReply(userStatus[index],message);
-  Symphony.sendMessage( message.stream.streamId, questionTemplate[index], null, Symphony.MESSAGEML_FORMAT);
+    parseUserReply(userStatus[id], messageText);
   }
+  questionString += '<br/>' + questionTemplate[userStatus[id]];
+  return questionString;
+}
+
+sendMessage = (msgObj, text) => {
+  Symphony.sendMessage(msgObj.stream.streamId, text, null, Symphony.MESSAGEML_FORMAT);
 }
 
 /* Callback function when the BOT hears a request */
-const botHearsRequest = ( event, messages ) => {
+const botHearsRequest = (event, messages) => {
 
-    messages.forEach( (message, index) => {
+  messages.forEach((message, index) => {
 
-      let doc = nlp(message.messageText);
+    let doc = nlp(message.messageText);
 
+    /* Find grettings */
+    let doc_grettings = doc.match('(hello|hi|bonjour)').out('tags');
+    let doc_help = doc.match('(test)').out('tags');
+    let doc_card = doc.match('(card)').out('tags');
+    let doc_table = doc.match('(table)').out('tags');
+    let doc_upload_file = doc.match('(upload)').out('tags');
+    let reply_message = '';
+    if (doc_grettings.length > 0) {
+      reply_message = 'Hello ' + message.user.firstName + ',<br/>' + 'Please help us to answer some questions.';
+      reply_message += getQuestion(message);
+      sendMessage(message, reply_message);
+    } else if (doc_help.length > 0) {
+      reply_message = "This is a <b>messageML</b>! message"; // No need Message ML Tag
+      sendMessage(message, reply_message);
+    } else if (doc_card.length > 0) {
+      reply_message = html_utils.generateCard('Card Header. Always visible.', 'Card Body. User must click to view it.', './images/favicon.png');
+      sendMessage(message, reply_message);
+    } else if (doc_table.length > 0) {
+      reply_message = html_utils.generateTable([['col1', 'col2', 'col3'], ['data1', 'data2', 'data3']]);
+      sendMessage(message, reply_message);
+    }
+    else if (doc_upload_file.length > 0) {
+      reply_message = "Click this <a href=\"http://localhost:8080\">link</a> to uploade file";
+      sendMessage(message, reply_message);
+    }
+    else {
+      reply_message = 'Sorry I don\'t know how to handle this yet. Please wait for our next available assistance to help with that';
+      sendMessage(message, reply_message);
+    }
 
-
-
-      /* Find grettings */
-      let doc_grettings = doc.match('(hello|hi|bonjour)').out('tags');
-      let doc_help = doc.match('(test)').out('tags');
-      let doc_card = doc.match('(card)').out('tags');
-      let doc_table = doc.match('(table)').out('tags');
-      let doc_upload_file = doc.match('(upload)').out('tags');
-      let reply_message = '';
-      if (doc_grettings.length>0) {
-        reply_message = 'Hello :' + message.messageText.toString() + 'Please help us to answer some questions.';
-        /*
-        for (var key1 in message.user) {
-          reply_message += `\n, ${key1}: ${message.user[key1]}`;
-        }
-        */
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-        userStatus = checkUser(message.user.userId,message.messageText.toString());
-
-
-      } else if (doc_help.length>0) {
-        reply_message = "This is a <b>messageML</b>! message"; // No need Message ML Tag
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-      } else if(doc_card.length>0) {
-        reply_message = html_utils.generateCard('Card Header. Always visible.', 'Card Body. User must click to view it.', './images/favicon.png');
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-      } else if(doc_table.length>0) {
-        reply_message = html_utils.generateTable([['col1', 'col2', 'col3'], ['data1', 'data2', 'data3']]);
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-      } 
-      else if(doc_upload_file.length>0) {
-        reply_message = "Click this <a href=\"http://localhost:8080\">link</a> to uploade file";
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-      }
-      else {
-        reply_message = 'Sorry I don\'t know how to handle this yet. Please wait for our next available assistance to help with that';
-        Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-      }
-
-     userStatus = checkUser(message.user.id,message);
-      /* Detect & analyze request */
-      SymphonyBotNLP.findPattern( doc, message );
-
-
-    })
+    /* Detect & analyze request */
+    SymphonyBotNLP.findPattern(doc, message);
+  })
 }
 
 /* Initialize BOT to Symphony */
-Symphony.initBot(__dirname + '/config.json').then( (symAuth) => {
-  Symphony.getDatafeedEventsService( botHearsRequest );
+Symphony.initBot(__dirname + '/config.json').then((symAuth) => {
+  Symphony.getDatafeedEventsService(botHearsRequest);
 })
 
 var http = require('http');
@@ -112,9 +108,9 @@ http.createServer(function (req, res) {
         res.write('File uploaded and moved!');
         res.end();
       });
- });
+    });
   } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(html_utils.generateUploadForm());
     return res.end();
   }
