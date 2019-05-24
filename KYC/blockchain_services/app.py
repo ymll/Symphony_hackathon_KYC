4be03e31485b3d71d953d8a47b2a4124093fd0e8
Lily Yung <ymll@live.hk>
2019-05-24 14:15:19 +0800

    verify and pending endpoint

--- KYC/blockchain_services/app.py
+++ KYC/blockchain_services/app.py
@@ -10,6 +10,8 @@ import json
 app = Flask(__name__)
 
 USERS_JSON = os.path.dirname(os.path.realpath(__file__)) + '/data/user.json'
+ONBOARDING_ASSIGNMENT_JSON = os.path.dirname(os.path.realpath(__file__)) + '/data/onboarding_assignment.json'
+VERIFY_LOG = os.path.dirname(os.path.realpath(__file__)) + '/data/verify_log.json'
 
 @app.route('/')
 @app.route('/hello/<name>')
@@ -46,7 +48,48 @@ def get_user_info():
 @app.route('/api/v1/user/create', methods = ['POST'])
 def create_user():
     data = request.form
-    return
+    # store the info to json
+    return make_response(jsonify(data), 200)
+
+
+@app.route('/api/v1/operation/pending', methods = ['GET'])
+def get_pending_list():
+    assignee_id = request.args.get('assignee_id')
+    if assignee_id is None:
+        return make_response('No assignee_id is given', 400)
+    try:
+        if ONBOARDING_ASSIGNMENT_JSON:
+            with open(ONBOARDING_ASSIGNMENT_JSON, encoding='utf-8') as data_file:
+                assignments = json.loads(data_file.read())
+            for assignment in assignments:
+                if assignment['assignee_id'] == int(assignee_id):
+                    return make_response(jsonify(assignment['pending_tasks']), 200)
+    except Exception as e:
+        return make_response('You are not a part of the onboarding team' , 401)
+
+
+@app.route('/api/v1/user/verify', methods = ['POST'])
+def verify_user():
+    verifier_id = request.args.get('verifier_id')
+    user_id = request.args.get('user_id')
+    if verifier_id is None:
+        return make_response('No verifier_id is given', 400)
+    if user_id is None:
+        return make_response('No user_id is given', 400)
+    try:
+        if VERIFY_LOG:
+            with open(VERIFY_LOG, encoding='utf') as data_file:
+                logs = json.loads(data_file.read())
+                logs.append({
+                    "verifier_id": verifier_id,
+                    "user_id": user_id
+                })
+            with open(VERIFY_LOG, 'w', encoding='utf-8') as fout:
+                json.dump(logs, fout)
+        return make_response('Success', 200)
+    except Exception as e:
+        return make_response('Failed (Error:' + str(e) + ')', 400)
+
 
 
 if __name__ == '__main__':
